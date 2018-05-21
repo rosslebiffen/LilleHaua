5cb05c13c632bacdc664267c72b93b313f2f5aee
rosslebiffen <jone.torgersen@gmail.com>
2018-05-21 20:15:45 +0200

    commit 3

--- app/src/main/java/com/example/jonet/lillehaua/FoodDetail.java
+++ app/src/main/java/com/example/jonet/lillehaua/FoodDetail.java
@@ -49,7 +49,7 @@ public class FoodDetail extends AppCompatActivity implements RatingDialogListene
     ElegantNumberButton numberButton;
 
 
-    String foodId="";
+    String foodId = "";
 
     FirebaseDatabase database;
     DatabaseReference foods;
@@ -75,31 +75,29 @@ public class FoodDetail extends AppCompatActivity implements RatingDialogListene
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent cartIntent = new Intent (FoodDetail.this,Cart.class);
+                Intent cartIntent = new Intent(FoodDetail.this, Cart.class);
                 startActivity(cartIntent);
             }
         });
 
         //InitView
-        numberButton = (ElegantNumberButton)findViewById(R.id.number_button);
+        numberButton = (ElegantNumberButton) findViewById(R.id.number_button);
         btnCart = (CounterFab) findViewById(R.id.btnCart);
-        btnRating = (FloatingActionButton)findViewById(R.id.btn_rating);
-        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        btnRating = (FloatingActionButton) findViewById(R.id.btn_rating);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
 
         btnRating.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showRatingDialog();
-                
+
             }
         });
 
 
-
-        btnCart.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View view)
-            {
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 new Database(getBaseContext()).addToCart(new Order(
                         Common.currentUser.getPhone(),
                         foodId,
@@ -113,30 +111,26 @@ public class FoodDetail extends AppCompatActivity implements RatingDialogListene
 
             }
         });
-        btnCart.setCount(new  Database(this).getCountCart(Common.currentUser.getPhone()));
+        btnCart.setCount(new Database(this).getCountCart(Common.currentUser.getPhone()));
 
-        food_description = (TextView)findViewById(R.id.food_description);
-        food_name = (TextView)findViewById(R.id.food_name);
-        food_price = (TextView)findViewById(R.id.food_price);
-        img_food = (ImageView)findViewById(R.id.img_food);
+        food_description = (TextView) findViewById(R.id.food_description);
+        food_name = (TextView) findViewById(R.id.food_name);
+        food_price = (TextView) findViewById(R.id.food_price);
+        img_food = (ImageView) findViewById(R.id.img_food);
 
-        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing);
         collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppbar);
         collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
 
         //get food id from intent
 
-        if(getIntent()!=null)
+        if (getIntent() != null)
             foodId = getIntent().getStringExtra("FoodId");
-        if(!foodId.isEmpty())
-        {
-            if(Common.isConnectedToInternet(getBaseContext()))
-            {
+        if (!foodId.isEmpty()) {
+            if (Common.isConnectedToInternet(getBaseContext())) {
                 getDetailFood(foodId);
                 getRatingFood(foodId);
-            }
-            else
-            {
+            } else {
                 Toast.makeText(FoodDetail.this, "Please check your internet connection ", Toast.LENGTH_SHORT).show();
                 return;
             }
@@ -147,17 +141,17 @@ public class FoodDetail extends AppCompatActivity implements RatingDialogListene
     private void getRatingFood(String foodId) {
         com.google.firebase.database.Query foodRating = ratingTbl.orderByChild("foodId").equalTo(foodId);
         foodRating.addValueEventListener(new ValueEventListener() {
-            int count=0, sum=0;
+            int count = 0, sum = 0;
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot postSnapshot:dataSnapshot.getChildren())
-                {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     Rating item = postSnapshot.getValue(Rating.class);
-                    sum+=Integer.parseInt(item.getRateValue());
+                    sum += Integer.parseInt(item.getRateValue());
                     count++;
                 }
-                if(count != 0){
-                    float average = sum/count;
+                if (count != 0) {
+                    float average = sum / count;
                     ratingBar.setRating(average);
                 }
             }
@@ -169,35 +163,31 @@ public class FoodDetail extends AppCompatActivity implements RatingDialogListene
         });
     }
 
-    private void showRatingDialog()
-    {
-        new  AppRatingDialog.Builder()
-               .setPositiveButtonText("Submit")
-               .setNegativeButtonText("Cancel")
-               .setNoteDescriptions(java.util.Arrays.asList("Very Bad", "Not Good", "Quite Ok", "Very Good", "Excellent"))
-               .setDefaultRating(1)
-               .setTitle("Rate this food")
-               .setDescription("Please select stars and give your feedback")
-               .setTitleTextColor(R.color.colorPrimary)
-               .setDescriptionTextColor(R.color.colorPrimary)
-               .setHint("Any comments?")
-               .setHintTextColor(R.color.colorAccent)
-               .setCommentTextColor(android.R.color.white)
-               .setCommentBackgroundColor(R.color.colorPrimaryDark)
-               .setWindowAnimation(R.style.RatingDialogFadeAnim)
-               .create(FoodDetail.this)
-               .show();
+    private void showRatingDialog() {
+        new AppRatingDialog.Builder()
+                .setPositiveButtonText("Submit")
+                .setNegativeButtonText("Cancel")
+                .setNoteDescriptions(java.util.Arrays.asList("Very Bad", "Not Good", "Quite Ok", "Very Good", "Excellent"))
+                .setDefaultRating(1)
+                .setTitle("Rate this food")
+                .setDescription("Please select stars and give your feedback")
+                .setTitleTextColor(R.color.colorPrimary)
+                .setDescriptionTextColor(R.color.colorPrimary)
+                .setHint("Any comments?")
+                .setHintTextColor(R.color.colorAccent)
+                .setCommentTextColor(android.R.color.white)
+                .setCommentBackgroundColor(R.color.colorPrimaryDark)
+                .setWindowAnimation(R.style.RatingDialogFadeAnim)
+                .create(FoodDetail.this)
+                .show();
 
     }
 
 
-
-
-
     private void getDetailFood(String foodId) {
-        foods.child(foodId).addValueEventListener(new ValueEventListener(){
+        foods.child(foodId).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot datasnapshot){
+            public void onDataChange(DataSnapshot datasnapshot) {
                 currentFood = datasnapshot.getValue(Food.class); //S or s
 
                 //set image
